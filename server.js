3b917c9b5be93513cbd64b84921b7dd1a326754c
davidnoah <david.t.noah@gmail.com>
2017-05-06 20:19:11 -0700

    added mongodb

--- server.js
+++ server.js
@@ -3,15 +3,26 @@ const app = express();
 
 const path = require('path');
 const cors = require('cors');
+const db = require('./db');
+
+const MongoClient = require('mongodb').MongoClient;
+const URL = 'mongodb://admin:password@ds133271.mlab.com:33271/box-era-db';
 
 app.use(cors());
-
+app.use(require('./router'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Magic is happening...');
+db.connect(URL, (err) => {
+  if (err) {
+    console.log('Fuck! DB wont connect');
+    process.exit(1);
+  } else {
+    app.listen(3000, () => {
+      console.log('Magic is happening...');
+    });
+  }
 });
