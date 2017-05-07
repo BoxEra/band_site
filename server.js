9de2ff4af12a62277cecc9d1b82db45ceb16bf9f
davidnoah <david.t.noah@gmail.com>
2017-05-06 19:48:40 -0700

    began server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Magic is happening...');
+});
