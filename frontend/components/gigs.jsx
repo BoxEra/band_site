225c6761a3f0b1d3d49a10169f4421593c978265
Joe Thomas <joe.p.thomas92@gmail.com>
2017-05-15 21:05:04 -0700

    get and display tour dates on gig page

--- frontend/components/gigs.jsx
+++ frontend/components/gigs.jsx
@@ -1,14 +1,43 @@
 import React from 'react';
+import { ajax } from 'jquery';
 
 class Gigs extends React.Component {
   constructor() {
     super();
+
+    this.state = {
+      tourDates: []
+    };
+  }
+
+  componentWillMount() {
+    ajax({
+      method: "GET",
+      url: `${location.origin}/getTourDates`
+    }).then((res) => {
+      let tourDates = res.tourDates.sort((a,b) => a.date < b.date ? -1 : 1);
+      this.setState({tourDates});
+    });
   }
 
   render() {
+    let tourDates = this.state.tourDates.map((gig, idx) => {
+      return (
+        <div className='tourDate' key={idx}>
+          <div>{gig.date.split('-').slice(0,2).join('/')}</div>
+          <div>{gig.venue}</div>
+          <div>{gig.location}</div>
+          <a href={gig.link} className={gig.link ? '' : 'deadLink'}>Link</a>
+        </div>
+      );
+    });
+
     return(
       <div>
-        <h1>GIGS</h1>
+        <h1>UPCOMING GIGS</h1>
+        <ul className='toureDates'>
+          {tourDates}
+        </ul>
       </div>
     );
   }
