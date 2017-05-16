82a02b63a6dc1aaff6f27337b09953cba74c1a93
davidnoah <david.t.noah@gmail.com>
2017-05-15 21:40:32 -0700

    commmitt

--- frontend/pages/Home/home.jsx
+++ frontend/pages/Home/home.jsx
@@ -6,7 +6,7 @@ import MusicPlayer from './music_player';
 import ReactPlayer from 'react-player';
 
 // actions
-import * as Util from '../actions/actions';
+import * as Util from '../../actions/actions';
 
 class Home extends React.Component {
   constructor(props) {
