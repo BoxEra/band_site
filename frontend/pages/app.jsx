34d02aeec46bd6e3b22b087405ed5226d83ba69c
davidnoah <david.t.noah@gmail.com>
2017-05-22 22:57:32 -0700

    splash and scroll

--- frontend/pages/app.jsx
+++ frontend/pages/app.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import { Route } from 'react-router-dom';
 import { withRouter } from 'react-router';
+import Scroll from 'react-scroll';
+const scroll = Scroll.animateScroll;
 
 import Home from './Home/home.jsx';
 import AboutUs from './AboutUs/about_us.jsx';
@@ -12,24 +14,29 @@ import Header from '../components/Header/header';
 
 import './app.scss';
 
-const Splash = () => (
+const Splash = ({ scroll }) => (
   <div className={'splashWrapper'}>
     <img id='header-image' src='https://s3.amazonaws.com/boxera/box_era_stylized_large.png' />
+    <div className='header-title'><strong>THE KNOX EP</strong> OUT NOW</div>
+    <div className='scroll-container'>
+      <div className='scroll-text' onClick={scroll}>SCROLL FOR MORE</div>
+      <i className="fa fa-angle-down fa-3x" onClick={scroll} style={{ariaHidden: "true"}}></i>
+    </div>
   </div>
 );
 
 class App extends Component {
 
-  // scrollToHeader() => {
-  //   return;
-  // };
+  scrollToHeader() {
+    scroll.scrollTo(680);
+  }
 
   render() {
     const isHomePage = (window.location.pathname === '/');
 
     return (
       <div>
-        {isHomePage && <Splash />}
+        {isHomePage && <Splash scroll={this.scrollToHeader}/>}
         <div ref={'header'}>
           <Header />
         </div>
