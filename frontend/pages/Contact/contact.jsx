7dcd34dcea14abee922e96e3c6b4c762591c6182
davidnoah <david.t.noah@gmail.com>
2017-05-25 20:19:40 -0700

    contact form

--- frontend/pages/Contact/contact.jsx
+++ frontend/pages/Contact/contact.jsx
@@ -3,10 +3,34 @@ import React from 'react';
 import './contact.scss';
 
 class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      message: ''
+    };
+  }
+
+  handleSubmit(event) {
+
+  }
+
   render() {
     return(
       <div className={'contactWrapper'}>
         <h1 className={'contactHeader'}>CONTACT</h1>
+        <div className={'contactText'}>
+          Integer mattis aliquam justo sed cursus. Vestibulum vehicula metus quis sem imperdiet, tincidunt ornare lectus dictum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubiliaCurae; Vestibulum accumsan felis hendrerit diam ornare hendrerit in in magna.
+        </div>
+        <div className='contactSenderContainer'>
+          <input className='contactSenderForm' id='name' placeholder='Name' />
+          <input className='contactSenderForm' id='email' placeholder='Email' />
+        </div>
+        <textarea className='contactSenderMessage' id='message' placeholder='Message' />
+        <div>
+          <button className='contactSendButton' onClick={this.handleSubmit}>Send</button>
+        </div>
       </div>
     );
   }
