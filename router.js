0dfd01f04dc6fae71512055894c0313eea774749
davidnoah <david.t.noah@gmail.com>
2017-06-05 16:57:45 -0700

    add mail client

--- router.js
+++ router.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const db = require('./db.js');
 
+const nodemailer = require('nodemailer');
+
+
 router.get('/getMusic', (req, res) => {
   let collection = db.get().collection('music');
 
@@ -24,6 +27,34 @@ router.get('/getTourDates', (req, res) => {
 
   collection.find().toArray((err, docs) => {
     res.json({tourDates: docs});
+  });
+});
+
+router.get('/sendEmail', (req, res) => {
+  const smtpTrans = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+          user: "contact@boxeramusic.com",
+          pass: "sweetCreamFunk"
+      }
+  });
+
+  const mailOpts = {
+     from: 'test@example.com', 
+     to: 'contact@boxeramusic.com',
+     subject: 'Website contact form',
+     text: 'test'
+ };
+
+ smtpTrans.sendMail(mailOpts, function (error, response) {
+      //Email not sent
+      if (error) {
+          res.status(500).json(error);
+      }
+      //Yay!! Email sent
+      else {
+          res.json(response);
+      }
   });
 });
 
