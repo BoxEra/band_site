49509e563dd14c7ff7e1346e32b0f4977c89630e
davidnoah <david.t.noah@gmail.com>
2017-06-08 10:05:32 -0700

    add contact email link

--- frontend/pages/Contact/contact.jsx
+++ frontend/pages/Contact/contact.jsx
@@ -20,8 +20,6 @@ class Contact extends React.Component {
     return(
       <div className={'contactWrapper'}>
         <h1 className={'contactHeader'}>CONTACT</h1>
-        <div className={'contactText'}>
-        </div>
         <div className='contactSenderContainer'>
           <input className='contactSenderForm' id='name' placeholder='Name' />
           <input className='contactSenderForm' id='email' placeholder='Email' />
@@ -31,6 +29,7 @@ class Contact extends React.Component {
           <button className='contactSendButton' onClick={this.handleSubmit}>Send</button>
         </div>
         <img className={'spacecraft'} src={'https://s3.amazonaws.com/boxera/spacecraft_large.png'}></img>
+        <div className={'contactText'}>or email us at <a className={'mailLink'} href='mailto:contact@boxeramusic.com'>contact@boxeramusic.com</a></div>
       </div>
     );
   }
